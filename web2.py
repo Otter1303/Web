025deaec9c95027b07239f588348b351ddca1f8d
Otter1303 <111745765+Otter1303@users.noreply.github.com>
2022-08-22 18:08:49 +0700

    Add files via upload

---
+++ web2.py
@@ -0,0 +1,15 @@
+import streamlit
+a = streamlit.set_page_config(page_title="Phimm",page_icon="clapperboard.ico") 
+a = streamlit.header("Phim hay o dayyy!")
+col1,col2 = streamlit.columns(2) 
+# (2) la so luong cot
+a = streamlit.sidebar()
+b = streamlit.sidebar.selectbox("kinh di")
+with col1:
+    a = streamlit.write("Harry Potter")
+    a = streamlit.video("https://www.youtube.com/watch?v=VyHV0BRtdxo")
+    a = streamlit.write("[Watch hereee](https://xemphim.vip/watch/195)")
+with col2:
+    a = streamlit.write("Catch me if you can")
+    a = streamlit.video("https://www.youtube.com/watch?v=s-7pyIxz8Qg")
+    a = streamlit.write("[Watch hereee](https://xemphim.vip/watch/1925)")
